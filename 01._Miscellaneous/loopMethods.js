0b2c0f402d8ee684fc97e1cf779578b83c435c29
SoerenTP <sope0001@stud.kea.dk>
2025-02-14 08:44:20 +0100

    loopMethods.js

---
+++ 01._Miscellaneous/loopMethods.js
@@ -0,0 +1,8 @@
+// forEach, map, filter, reduce, findIndex, find
+
+const myNumbers = [1, 2, 3, 4, 5, 6];
+
+// assignment: get a list of the numbers doubled
+
+const doubledNumbers = myNumbers.map(num => num * 2);
+console.log(doubledNumbers);
