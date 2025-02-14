13dc32a826a68afd5416254f7c3447156a3c1564
SoerenTP <sope0001@stud.kea.dk>
2025-02-14 10:34:58 +0100

    FingerBonesServer

--- 03._Finger_Bones_REST_API/app.js
+++ 03._Finger_Bones_REST_API/app.js
@@ -1,22 +1,27 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-let fingerBones = [
-    { id: "1", name: "Proximal Phalanx", finger: "Index", length: "3.8cm" },
-    { id: "2", name: "Middle Phalanx", finger: "Middle", length: "2.5cm" },
-    { id: "3", name: "Distal Phalanx", finger: "Thumb", length: "2.1cm" }
+const fingerBones = [
+    {
+        id: 1,
+        artistName: "Phalange Knowles"
+    },
+    {
+        id: 2,
+        artistName: "Distal Phalange"
+    }
 ];
 
-app.get('/fingerbones', (req, res) => {
+app.get("/fingerbones", (req, res) => {
     res.json({
         status: "success",
         data: fingerBones
     });
 });
 
-app.get('/fingerbones/:id', (req, res) => {
-    const fingerBone = fingerBones.find(bone => bone.id === req.params.id);
+app.get("/fingerbones/:id", (req, res) => {
+    const fingerBone = fingerBones.find((fingerBone) => fingerBone.id === +req.params.id);
     
     if (!fingerBone) {
         return res.status(404).json({
