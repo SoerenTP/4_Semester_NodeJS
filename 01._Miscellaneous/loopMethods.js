68bd1daf8fe1476c14c57931a3394887403bd713
SoerenTP <sope0001@stud.kea.dk>
2025-02-14 09:41:39 +0100

    FingerBones_loopMethods

--- 01._Miscellaneous/loopMethods.js
+++ 01._Miscellaneous/loopMethods.js
@@ -6,3 +6,35 @@ const myNumbers = [1, 2, 3, 4, 5, 6];
 
 const doubledNumbers = myNumbers.map(num => num * 2);
 console.log(doubledNumbers);
+
+const satellites = [
+    {
+        name: "International Space Station",
+        height: 12.000
+    },
+    {
+        name: "MIR",
+        height: 0
+    },
+    {
+        name: "James Webb",
+        height: 27.000
+    }
+];
+
+//assignment: lower the height for all satellites by 3.000 except for MIR
+
+const adjustedSatellites = satellites.map((satellite) => ({
+    height: satellite.name === "MIR" ? satellite.height : satellite.height - 3000,
+    name: satellite.name
+}));
+
+console.log(adjustedSatellites);
+
+// assignment: make a list that reacts as many times as above with thumbs up
+
+const listOfReactions = ["thumbs up", "thumbs down"];
+
+const updatedReactions = listOfReactions.map((reaction) => "thumbs up");
+
+console.log(listOfReactions);
