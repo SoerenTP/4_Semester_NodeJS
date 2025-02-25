024cb1fb5ef9957ebd89245638096ae68fd26c55
SoerenTP <sope0001@stud.kea.dk>
2025-02-25 13:39:39 +0100

    FingerBoneAPI

--- 03._Finger_Bones_REST_API/app.js
+++ 03._Finger_Bones_REST_API/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const fingerBones = [
     {
         id: 1,
@@ -9,34 +11,84 @@ const fingerBones = [
     },
     {
         id: 2,
-        artistName: "Distal Phalange"
+        name: "Distal Phalange"
     }
 ];
+let nextId = 3;
+
 
 app.get("/fingerbones", (req, res) => {
-    res.json({
-        status: "success",
-        data: fingerBones
-    });
+    res.send({ data: fingerBones });
 });
-// Get handler || Route Handler
+
 app.get("/fingerbones/:id", (req, res) => {
-    const fingerBone = fingerBones.find((fingerBone) => fingerBone.id === +req.params.id);
-    
-    if (!fingerBone) {
-        return res.status(404).json({
-            status: "error",
-            message: "FingerBone not found"
-        });
+    const providedFingerBoneId = Number(req.params.id);
+    const foundFingerBone = fingerBones.find((fingerBone) => fingerBone.id === providedFingerBoneId);
+
+    if (!foundFingerBone) {
+        res.status(404).send({ error: `No finger bones found with id ${providedFingerBoneId}` });
+    } else {
+        res.send({ data: foundFingerBone });
     }
-    
-    res.json({
-        status: "success",
-        data: fingerBone
-    });
 });
+
+// prefix increment
+// console.log(++currentId);
+// console.log(currentId);
+
+// postfix increment
+// console.log(currentId++);
+// console.log(currentId);
+
+app.post("/fingerbones", (req, res) => {
+    const newFingerBone = req.body;
+    newFingerBone.id = nextId++;
+
+    fingerBones.push(newFingerBone);
+
+    res.send({ data: newFingerBone });
+});
+
+
+app.patch("/fingerbones/:id", (req, res) => {
+    const providedFingerBoneId = Number(req.params.id);
+    const foundFingerBoneIndex = fingerBones.findIndex((fingerBone) => fingerBone.id === providedFingerBoneId);
+    
+    if (foundFingerBoneIndex === -1) {
+        res.status(404).send({ error: `No finger bones found with id ${providedFingerBoneId}` });
+    } else {
+        const existingFingerBone = fingerBones[foundFingerBoneIndex];
+        
+        const newFingerBone = { ...existingFingerBone, ...req.body, id: existingFingerBone.id };
+
+        fingerBones[foundFingerBoneIndex] = newFingerBone;
+
+        res.send({ data: newFingerBone });
+    }
+});
+
+app.delete("/fingerbones/:id", (req, res) => {
+    const providedFingerBoneId = Number(req.params.id);
+    const foundFingerBoneIndex = fingerBones.findIndex((fingerBone) => fingerBone.id === providedFingerBoneId);
+    
+    if (foundFingerBoneIndex === -1) {
+        res.status(404).send({ error: `No finger bones found with id ${providedFingerBoneId}` });
+    } else {
+        fingerBones.splice(foundFingerBoneIndex, 1);
+        res.status(204).send();
+    }
+});
+
+// status codes
+// 2XX - Everything went well
+// 4XX - Client Error
+// 5XX - Server Error
 
 const PORT = 8080;
-app.listen(PORT);
-
-
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log("Error starting the server", error);
+        return;
+    }
+    console.log("Server is running on port", PORT);
+});
