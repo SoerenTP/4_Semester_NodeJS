d10c0f5bb7e52efef476b8e000fa00034112983c
SoerenTP <sope0001@stud.kea.dk>
2025-03-21 11:01:51 +0100

    Svelte

---
+++ 07._First-Svelte_Project/src/components/Counter/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+    let counter = 0;
+</script>
+
+<h3 id="counter">{counter}</h3>
+
+<button onclick={() => counter++}>Increment</button>
+<button onclick={() => counter--}>Decrement</button>
+
+<style>
+    #counter {
+        color:yellow;
+    }
+</style>
---
+++ 07._First-Svelte_Project/src/components/Messages/Messages.svelte
@@ -0,0 +1,18 @@
+<script>
+    const messages = $state([]);
+
+    let messageInput = $state("");
+
+    function addMessage(){
+        messages.push(messageInput);
+        messageInput = "";
+    }
+</script>
+
+<h1>Messages</h1>
+
+<p>{messages}</p>
+
+<input bind:value={messageInput} placeholder="Type you message...">
+
+<button onclick={addMessage}>Add Message</button>
